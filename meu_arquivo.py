353b7b92d6b829f70652328e6dea02a4ed2af9b4
Marcio Cavalcante <marcio.cavalcante@8899-MB.local>
2023-10-17 21:41:50 -0300

    Acrescentando o CSV do Titanic e instalando as bibliotes

--- meu_arquivo.py
+++ meu_arquivo.py
@@ -1,3 +1,8 @@
-def soma(x, y):
-    return x + y 
+import pandas as pd
+import numpy as np
+import seaborn as sns
 
+# Load Titanic dataset
+df_titanic = sns.load_dataset('titanic')
+
+df_titanic.to_csv('titanic.csv')
